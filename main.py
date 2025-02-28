a148a59c56ce864fd2c5514ff08a52fc59c06b4c
youssoupha-marega <youssouphamarega@gmail.com>
2025-02-28 17:08:31 -0500

    deploiement avec guithub action vers heroku

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def index():
 ## Executer la commande suivante pour voir l'application : uvicorn main:app --reload --host 0.0.0.0 --port 8000
 ### l'app va etre disponible via le navigateur http://127.0.0.1:8000/
 ## Pour la documentation l'app disponible http://127.0.0.1:8000/docs
-
+## test 2
